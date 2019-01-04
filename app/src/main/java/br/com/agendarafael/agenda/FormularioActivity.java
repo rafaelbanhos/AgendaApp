691e2669d901f27d761ab9d7fdf8d7624a971671
rafaelbanhos <rafaelbanhos@gmail.com>
2019-01-04 17:20:35 +0000

    Second commit

--- app/src/main/java/br/com/agendarafael/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/agendarafael/agenda/FormularioActivity.java
@@ -6,31 +6,34 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import br.com.agendarafael.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
+        helper = new FormularioHelper(this);
+
         Button botaoSalvar = findViewById(R.id.formulario_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Aluno salvo!", Toast.LENGTH_SHORT).show();
+                Aluno aluno = helper.pegaAluno();
+                Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
+
                 finish();
             }
         });
-    }
-
-    @Override //definir os itens do menu na parte de cima
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
